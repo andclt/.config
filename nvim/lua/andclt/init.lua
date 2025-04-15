99333467aa6c62fe2b20b239a2cb7eadb085d899
andclt <andreaculot99@gmail.com>
2025-04-16 00:17:20 +0100

    handle work plugins

--- nvim/lua/andclt/init.lua
+++ nvim/lua/andclt/init.lua
@@ -1,3 +1,9 @@
 require("andclt.mappings")
 require("andclt.lazy_init")
 require("andclt.set")
+local helpers = require("andclt.helpers")
+
+-- I keep work specific configuration in a private work repo and connect it via symlinks.
+if helpers.isModuleAvailable("andclt.stripe") then
+  require("andclt.stripe").setup()
+end
