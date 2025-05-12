8d436689283e44323eb7424a2efa189de6521a2e
andclt <andreaculot99@gmail.com>
2025-05-13 00:19:39 +0100

    fix livegrep

--- nvim/lua/andclt/lazy/telescope.lua
+++ nvim/lua/andclt/lazy/telescope.lua
@@ -11,14 +11,56 @@ return {
     },
 
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup({
+            defaults = {
+                vimgrep_arguments = {
+                    "rg",
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                    "--smart-case",
+                    "--hidden",
+                    "--glob", "!.git/*",
+                },
+                file_ignore_patterns = {
+                    "node_modules",
+                    ".git",
+                },
+                preview = {
+                    treesitter = true,
+                },
+                layout_config = {
+                    width = 0.9,
+                    height = 0.9,
+                    preview_width = 0.6,
+                },
+            },
+            pickers = {
+                live_grep = {
+                    additional_args = function()
+                        return {"--hidden"}
+                    end,
+                    preview = true,
+                },
+            },
+        })
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+            local ok, err = pcall(builtin.live_grep, {
+                search_dirs = {vim.fn.getcwd()},
+                prompt_title = "Live Grep",
+                results_title = "Search Results",
+                preview = true,
+            })
+            if not ok then
+                vim.notify("Error in live_grep: " .. tostring(err), vim.log.levels.ERROR)
+            end
+        end, {})
     end
 }
 
